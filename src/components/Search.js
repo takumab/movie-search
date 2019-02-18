605358375158faff25dbb6880e875023e2241915
takumab <takumab@gmail.com>
2019-02-18 19:00:26 +0000

    Add test functionality to search input

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,12 +2,32 @@ import React, {Component} from 'react'
 
 class Search extends Component {
     state = {
+        value: '',
         films: []
+    }
+
+    handleChange = (event) => {
+        this.setState({value: event.target.value})
+    }
+
+    handleSubmit = (e) => {
+        alert(`A film title was submitted: ${this.state.value}`)
+        e.preventDefault();
     }
 
     render() {
         return (
-            <h1>Search Films</h1>
+            <div className="film-search">
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="text"
+                        placeholder="Enter title of film" 
+                        value={this.state.value} 
+                        onChange={this.handleChange}
+                    />
+                    <input type="submit" value="Search" />
+                </form>
+            </div>
         )
     }
 }
