02d04d0b9943ff3807871974f419b813fd9120ea
takumab <takumab@gmail.com>
2019-02-18 18:09:24 +0000

    Create simple render test for Film component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Film from './components/Film';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders Film component', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Film />, div)
+})
