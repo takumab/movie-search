dbb26d6eb3e801e3361e91eead5a54812f05d4e5
takumab <takumab@gmail.com>
2019-02-18 18:09:52 +0000

    Add Film component to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Film from './components/Film'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Film />
       </div>
     );
   }
